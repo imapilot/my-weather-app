d81d8edd33d81395ac1b2d84a5118eea0a83b96f
Neville Smythe <nevillesmythe@nevilles-imac.local>
2016-02-17 13:52:31 +1000

    Initial Commit

---
+++ My Weather App/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  My Weather App
+//
+//  Created by Neville Smythe on 17/02/2016.
+//  Copyright © 2016 Neville Smythe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
